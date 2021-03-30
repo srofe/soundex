43f60982cda5f04715f5bb7a900652bcca18ad3e
Simon Rofe <simon.rofe@ieee.org>
2021-03-30 17:04:09 +1000

    Refactor to extract a new method
    This makes the intent of the encode() method more explicit.

--- soundex_tests/SoundexTest.cc
+++ soundex_tests/SoundexTest.cc
@@ -3,6 +3,11 @@
 class Soundex {
 public:
     std::string encode(const std::string& word) const {
+        return zeroPad(word);
+    }
+
+private:
+    std::string zeroPad(const std::string& word) const {
         return word + "000";
     }
 };
