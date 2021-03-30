513be1ffc87ede5df61c547c02774190d66c365c
Simon Rofe <simon.rofe@ieee.org>
2021-03-30 17:01:08 +1000

    Pad encoded string with zeros
    This uses an additional test and requires the original test to be
    modified to account for the new functionality.

--- soundex_tests/SoundexTest.cc
+++ soundex_tests/SoundexTest.cc
@@ -3,14 +3,22 @@
 class Soundex {
 public:
     std::string encode(const std::string& word) const {
-        return "A";
+        return word + "000";
     }
 };
 
 #include "gmock/gmock.h"
 
+using ::testing::Eq;
+
 TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
     Soundex soundex;
     auto encoded = soundex.encode("A");
-    ASSERT_THAT(encoded, testing::Eq("A"));
+    ASSERT_THAT(encoded, Eq("A000"));
+}
+
+TEST(SoundexEncoding, PadsWithZerosToEnsureThreeDigits) {
+    Soundex soundex;
+    auto encoded = soundex.encode("I");
+    ASSERT_THAT(encoded, Eq("I000"));
 }
