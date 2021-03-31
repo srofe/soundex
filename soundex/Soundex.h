2e66cdd6f3fea8d461093fce63e8140c7f140aec
Simon Rofe <simon.rofe@ieee.org>
2021-03-31 11:21:22 +1000

    Address duplicate encodings
    This accounts for cases where adjacent letters encode to the same digit
    - as per the digit map.
    
    This required making a previously private method public to enable it to
    be used for testing.

--- soundex/Soundex.h
+++ soundex/Soundex.h
@@ -10,6 +10,20 @@ public:
         return zeroPad(head(word) + encodedDigits(tail(word)));
     }
 
+    std::string encodedDigit(char letter) const {
+        const std::unordered_map<char, std::string> encodings {
+                {'b', "1"}, {'f', "1"}, {'p', "1"}, {'v', "1"},
+                {'c', "2"}, {'g', "2"}, {'j', "2"}, {'k', "2"},
+                {'q', "2"}, {'s', "2"}, {'x', "2"}, {'z', "2"},
+                {'d', "3"}, {'t', "3"},
+                {'l', "4"},
+                {'m', "5"}, {'n', "5"},
+                {'r', "6"},
+        };
+        auto digit = encodings.find(letter);
+        return digit == encodings.end() ? "" : digit->second;
+    }
+
 private:
     static const size_t MaxCodeLength{4};
 
@@ -24,28 +38,20 @@ private:
     std::string encodedDigits(const std::string& word) const {
         std::string encoding;
         for (auto letter: word) {
-            if (isComplet(encoding)) break;
-            encoding += encodedDigit(letter);
+            if (isComplete(encoding)) break;
+            if (encodedDigit(letter) != lastDigit(encoding))
+                encoding += encodedDigit(letter);
         }
         return encoding;
     }
 
-    bool isComplet(const std::string& encoding) const {
-        return encoding.length() == MaxCodeLength -1;
+    std::string lastDigit(const std::string& encoding) const {
+        if (encoding.empty()) return "";
+        return std::string(1, encoding.back());
     }
 
-    std::string encodedDigit(char letter) const {
-        const std::unordered_map<char, std::string> encodings {
-                {'b', "1"}, {'f', "1"}, {'p', "1"}, {'v', "1"},
-                {'c', "2"}, {'g', "2"}, {'j', "2"}, {'k', "2"},
-                {'q', "2"}, {'s', "2"}, {'x', "2"}, {'z', "2"},
-                {'d', "3"}, {'t', "3"},
-                {'l', "4"},
-                {'m', "5"}, {'n', "5"},
-                {'r', "6"},
-        };
-        auto digit = encodings.find(letter);
-        return digit == encodings.end() ? "" : digit->second;
+    bool isComplete(const std::string& encoding) const {
+        return encoding.length() == MaxCodeLength -1;
     }
 
     std::string zeroPad(const std::string& word) const {
